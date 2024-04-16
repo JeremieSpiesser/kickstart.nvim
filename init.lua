05202e1e3fbedefa59ca9b5502c9ba051a773165
Jeremie Spiesser <jeremie.spiesser@qarnot-computing.com>
2024-04-16 11:20:47 +0200

    added keybinds + git blame plugin

--- init.lua
+++ init.lua
@@ -29,6 +29,12 @@ vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
+vim.keymap.set('i', '<C-u>', '<C-u>zz')
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set('i', '<C-d>', '<C-d>zz')
+vim.keymap.set('i', '<Esc><C-s>', ':w<CR>')
+vim.keymap.set('n', '<C-s>', ':w<CR>')
 
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
---
+++ lua/custom/plugins/git.lua
@@ -0,0 +1,4 @@
+vim.g.gitblame_virtual_text_column = 80
+return {
+  'f-person/git-blame.nvim',
+}
