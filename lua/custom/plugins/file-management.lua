26537f231d8193c419c018ea9837a5df0ca6364c
jerespi <jeremie.spiesser@gmail.com>
2024-07-12 23:18:58 +0200

    reintroduced oil

--- lua/custom/plugins/file-management.lua
+++ lua/custom/plugins/file-management.lua
@@ -1,11 +1,23 @@
---vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
+local open = false
+
+vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
+vim.keymap.set('n', '<leader>u', function()
+  if open then
+    open = false
+    require('oil').close()
+  else
+    open = true
+    require('oil').open()
+  end
+end, { desc = 'Close oil.nvim' })
+--
 --vim.keymap.set('n', '-', function()
 --  vim.cmd 'vsplit | wincmd l'
 --  require('oil').open()
 --end, { desc = 'Open parent directory' })
 return {
   'stevearc/oil.nvim',
-  enabled = false,
+  enabled = true,
   opts = {},
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function()
