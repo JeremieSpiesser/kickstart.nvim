d2a0b2c74152f676f47c0639b765876e9d6576f8
Jeremie Spiesser <jeremie.spiesser@qarnot-computing.com>
2024-07-15 11:52:43 +0200

    added git blame

--- lua/custom/plugins/git.lua
+++ lua/custom/plugins/git.lua
@@ -1,4 +1,5 @@
-vim.g.gitblame_virtual_text_column = 80
+vim.keymap.set('n', '<leader>cy', ':GitBlameCopyFileURL<CR>', { desc = 'Copy git blame file url' })
+vim.g.gitblame_virtual_text_column = 100
 return {
   'f-person/git-blame.nvim',
 }
