f6cf38fd5dd802a38f9b511f987c04b38ca0d905
Jeremie Spiesser <jeremie.spiesser@qarnot-computing.com>
2024-07-27 19:38:22 +0200

    added <leader>x to close buffer

--- lua/custom/plugins/bufferline.lua
+++ lua/custom/plugins/bufferline.lua
@@ -1,6 +1,7 @@
 vim.keymap.set('n', '<Tab>', ':BufferLineCycleNext<CR>')
 vim.keymap.set('n', '<S-Tab>', ':BufferLineCyclePrev<CR>')
 vim.keymap.set('n', '<leader>pc', ':BufferLinePickClose<CR>')
+vim.keymap.set('n', '<leader>x', ':bd<CR>')
 return {
   'akinsho/bufferline.nvim',
   version = '4.6.1',
