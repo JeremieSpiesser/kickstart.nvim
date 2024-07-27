5749584b0dc25a88b8623a21d47617264771923c
Jeremie Spiesser <jeremie.spiesser@qarnot-computing.com>
2024-07-27 19:37:47 +0200

    git stuff

--- lua/custom/plugins/bufferline.lua
+++ lua/custom/plugins/bufferline.lua
@@ -1,5 +1,6 @@
 vim.keymap.set('n', '<Tab>', ':BufferLineCycleNext<CR>')
 vim.keymap.set('n', '<S-Tab>', ':BufferLineCyclePrev<CR>')
+vim.keymap.set('n', '<leader>pc', ':BufferLinePickClose<CR>')
 return {
   'akinsho/bufferline.nvim',
   version = '4.6.1',
--- lua/custom/plugins/git.lua
+++ lua/custom/plugins/git.lua
@@ -1,5 +1,24 @@
 vim.keymap.set('n', '<leader>cy', ':GitBlameCopyFileURL<CR>', { desc = 'Copy git blame file url' })
 vim.g.gitblame_virtual_text_column = 100
 return {
-  'f-person/git-blame.nvim',
+  { 'f-person/git-blame.nvim' },
+  {
+    'kdheepak/lazygit.nvim',
+    cmd = {
+      'LazyGit',
+      'LazyGitConfig',
+      'LazyGitCurrentFile',
+      'LazyGitFilter',
+      'LazyGitFilterCurrentFile',
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
+    -- setting the keybinding for LazyGit with 'keys' is recommended in
+    -- order to load the plugin when the command is run for the first time
+    keys = {
+      { '<leader>lg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
+    },
+  },
 }
