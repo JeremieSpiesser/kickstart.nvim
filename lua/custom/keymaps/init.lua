7fd05fb2f3a32f377b5939a8d448acea20aab5c8
Jeremie Spiesser <jeremie.spiesser@qarnot-computing.com>
2024-08-21 18:02:12 +0200

    c-c = esc in insert mode

--- lua/custom/keymaps/init.lua
+++ lua/custom/keymaps/init.lua
@@ -1,9 +1,10 @@
 vim.keymap.set('n', '<C-u>', '<C-u>zz')
-vim.keymap.set('i', '<C-u>', '<C-u>zz')
+--vim.keymap.set('i', '<C-u>', '<C-u>zz')
 vim.keymap.set('n', '<C-d>', '<C-d>zz')
-vim.keymap.set('i', '<C-d>', '<C-d>zz')
+--vim.keymap.set('i', '<C-d>', '<C-d>zz')
 vim.keymap.set('i', '<Esc><C-s>', ':w<CR>')
 vim.keymap.set('n', '<C-s>', ':w<CR>')
 vim.keymap.set('n', '<leader>oi', 'va{Vo')
 
+vim.keymap.set('i', '<C-c>', '<Esc>')
 return {}
