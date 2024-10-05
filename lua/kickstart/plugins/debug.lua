77824337240c533f5aeec95bc282c3a3df6e3fe4
jerespi <jeremie.spiesser@gmail.com>
2024-10-06 00:43:19 +0200

    rr reverse debugger for rust

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -23,6 +23,7 @@ return {
 
     -- Add your own debuggers here
     'leoluz/nvim-dap-go',
+    'jonboh/nvim-dap-rr',
   },
   config = function()
     local dap = require 'dap'
@@ -43,6 +44,12 @@ return {
         -- Update this to ensure that you have the debuggers for the langs you want
         'delve',
       },
+    }
+    local cpptools_path = vim.fn.stdpath 'data' .. '/mason/packages/cpptools/extension/debugAdapters/bin/OpenDebugAD7'
+    dap.adapters.cppdbg = {
+      id = 'cppdbg',
+      type = 'executable',
+      command = cpptools_path,
     }
 
     -- Basic debugging keymaps, feel free to change to your liking!
@@ -86,5 +93,31 @@ return {
 
     -- Install golang specific config
     require('dap-go').setup()
+    -- Rust
+    local rr_dap = require 'nvim-dap-rr'
+    rr_dap.setup {
+      mappings = {
+        -- you will probably want to change these defaults so that they match
+        -- your usual debugger mappings. They will override the normal dap::<fun>
+        -- during rr debugging sessions
+        continue = '<F5>',
+        step_over = '<F2>',
+        step_out = '<F3>',
+        step_into = '<F1>',
+        reverse_continue = '<F8>', -- <S-F7>
+        reverse_step_over = '<F9>', -- <S-F8>
+        reverse_step_out = '<F10>', -- <S-F9>
+        reverse_step_into = '<F11>', -- <S-F10>
+        -- instruction level stepping
+        step_over_i = '<F12>', -- <C-F8>
+        step_out_i = '<F33>', -- <C-F8>
+        step_into_i = '<F34>', -- <C-F8>
+        reverse_step_over_i = '<F44>', -- <SC-F8>
+        reverse_step_out_i = '<F45>', -- <SC-F9>
+        reverse_step_into_i = '<F46>', -- <SC-F10>
+      },
+    }
+    dap.configurations.rust = { rr_dap.get_rust_config() }
+    dap.configurations.cpp = { rr_dap.get_config() }
   end,
 }
