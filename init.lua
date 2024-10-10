cea7f88b0eba805e587872d85e6a1cca9a0e0db7
jerespi <jeremie.spiesser@gmail.com>
2024-10-10 21:27:19 +0200

    theme

--- init.lua
+++ init.lua
@@ -496,11 +496,31 @@ require('lazy').setup({
     end,
   },
 
+  -- {
+  --   'folke/tokyonight.nvim',
+  --   priority = 1000,
+  --   init = function()
+  --     vim.cmd.colorscheme 'tokyonight-night'
+  --     vim.cmd.hi 'Comment gui=none'
+  --   end,
+  -- },
   {
-    'folke/tokyonight.nvim',
-    priority = 1000,
+    'navarasu/onedark.nvim',
+    enabled = true,
     init = function()
-      vim.cmd.colorscheme 'tokyonight-night'
+      require('onedark').setup {
+        style = 'darker',
+      }
+      vim.cmd.colorscheme 'onedark'
+      vim.cmd.hi 'Comment gui=none'
+    end,
+  },
+  {
+    'sainnhe/sonokai',
+    enabled = false,
+    init = function()
+      -- vim.g.sonokai_style = 'espresso'
+      vim.cmd.colorscheme 'sonokai'
       vim.cmd.hi 'Comment gui=none'
     end,
   },
