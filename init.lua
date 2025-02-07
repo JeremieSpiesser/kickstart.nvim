c366da53c3ed74b52e7fdeaddcc95914696cc3da
Jeremie Spiesser <jeremie.spiesser@qarnot-computing.com>
2025-02-07 17:21:02 +0100

    tokyonight is still the best

--- init.lua
+++ init.lua
@@ -598,14 +598,14 @@ require('lazy').setup({
     end,
   },
 
-  -- {
-  --   'folke/tokyonight.nvim',
-  --   priority = 1000,
-  --   init = function()
-  --     vim.cmd.colorscheme 'tokyonight-night'
-  --     vim.cmd.hi 'Comment gui=none'
-  --   end,
-  -- },
+  {
+    'folke/tokyonight.nvim',
+    priority = 1000,
+    init = function()
+      vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.hi 'Comment gui=none'
+    end,
+  },
   {
     'navarasu/onedark.nvim',
     enabled = true,
@@ -613,17 +613,17 @@ require('lazy').setup({
       require('onedark').setup {
         style = 'darker',
       }
-      vim.cmd.colorscheme 'onedark'
-      vim.cmd.hi 'Comment gui=none'
+      -- vim.cmd.colorscheme 'onedark'
+      -- vim.cmd.hi 'Comment gui=none'
     end,
   },
   {
     'sainnhe/sonokai',
-    enabled = false,
+    enabled = true,
     init = function()
       -- vim.g.sonokai_style = 'espresso'
-      vim.cmd.colorscheme 'sonokai'
-      vim.cmd.hi 'Comment gui=none'
+      -- vim.cmd.colorscheme 'sonokai'
+      -- vim.cmd.hi 'Comment gui=none'
     end,
   },
 
