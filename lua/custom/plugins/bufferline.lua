b3785d921c61ea24d87c2c028117869abd364724
Jeremie Spiesser <jeremie.spiesser@qarnot-computing.com>
2025-02-07 17:19:41 +0100

    improved buffer killing

--- lua/custom/plugins/bufferline.lua
+++ lua/custom/plugins/bufferline.lua
@@ -2,7 +2,7 @@ vim.keymap.set('n', '<Tab>', ':BufferLineCycleNext<CR>')
 vim.keymap.set('n', '<S-Tab>', ':BufferLineCyclePrev<CR>')
 vim.keymap.set('n', '<leader>pc', ':BufferLinePickClose<CR>')
 vim.keymap.set('n', '<leader>tp', ':BufferLineTogglePin<CR>')
-vim.keymap.set('n', '<leader>x', ':bd<CR>')
+vim.keymap.set('n', '<leader>x', ':bp|bd #<CR>')
 vim.keymap.set('n', '<leader>X', ':BufferLineCloseOthers<CR>')
 return {
   'akinsho/bufferline.nvim',
