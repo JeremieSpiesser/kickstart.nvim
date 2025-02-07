a1da317d1b68587db1e6c125242ea424eaca2427
Jeremie Spiesser <jeremie.spiesser@qarnot-computing.com>
2025-02-07 17:19:27 +0100

    ctrl a e : scroll with 4k instead of mousewheel

--- lua/custom/keymaps/init.lua
+++ lua/custom/keymaps/init.lua
@@ -5,8 +5,9 @@ vim.keymap.set('n', '<C-d>', '<C-d>zz')
 vim.keymap.set('i', '<Esc><C-s>', ':w<CR>')
 vim.keymap.set('n', '<C-s>', ':w<CR>')
 vim.keymap.set('n', '<leader>oi', 'va{Vo')
-vim.keymap.set('n', '<C-a>', '<ScrollWheelUp>')
-vim.keymap.set('n', '<C-e>', '<ScrollWheelDown>')
+vim.keymap.set('n', '<C-a>', '4k')
+vim.keymap.set('n', '<C-e>', '4j')
+
 
 vim.keymap.set('i', '<C-c>', '<Esc>')
 return {}
