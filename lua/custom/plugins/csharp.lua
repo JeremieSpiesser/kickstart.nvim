70088d39abb06fa752ede352fd0bcfa6258d2fdc
Jeremie Spiesser <jeremie.spiesser@qarnot-computing.com>
2025-04-05 15:17:53 +0200

    csharp stuff

---
+++ lua/custom/plugins/csharp.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    'iabdelkareem/csharp.nvim',
+    enabled = false,
+    dependencies = {
+      'williamboman/mason.nvim', -- Required, automatically installs omnisharp
+      'mfussenegger/nvim-dap',
+      'Tastyep/structlog.nvim', -- Optional, but highly recommended for debugging
+    },
+    config = function()
+      require('mason').setup() -- Mason setup must run before csharp
+      require('csharp').setup()
+    end,
+  }
+}
